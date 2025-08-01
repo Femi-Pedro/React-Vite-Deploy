df5e91d815df58cdd74712d6b090926524c19540
Oluwafemi <sowolefemi2@gmail.com>
2025-08-01 08:55:12 +0100

    ecommerce

---
+++ src/components/OurPolicy.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { assets } from './assets/assets'
+
+const OurPolicy = () => {
+  return (
+    <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 textcenter py-20 text-xs sm-text-sm md:text-base text-gray-700'>
+
+      <div>
+        <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt="" />
+        <p className='font-semibold'>Easy Exchannge policy</p>
+        <p className='ext-gray-400'>We offer hassle free exchange policy</p>
+      </div>  
+      <div>
+        <img src={assets.quality_icon} className='w-12 m-auto mb-5' alt="" />
+        <p className='font-semibold'>7 Days Return policy</p>
+        <p className='ext-gray-400'>We provide 7 days free return policy</p>
+      </div>  
+      <div>
+        <img src={assets.support_img} className='w-12 m-auto mb-5' alt="" />
+        <p className='font-semibold'>Best cutomer support</p>
+        <p className='ext-gray-400'>We provide 24/7 customer support</p>
+      </div>  
+
+    </div>
+  )
+}
+
+export default OurPolicy
