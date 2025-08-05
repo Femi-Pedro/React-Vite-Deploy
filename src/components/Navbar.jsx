c3d0c75a62d073b83cc830dce850a982056528e2
Oluwafemi <sowolefemi2@gmail.com>
2025-08-05 07:51:15 +0100

    Collections toggle update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import {assets} from './assets/assets'
 import { Link, NavLink } from 'react-router-dom' 
+import { ShopContext } from '../context/ShopContext'
 
 const Navbar = () => {
 
   const [visible, setVisible] = useState(false)
+
+  const {setShowSearch} = useContext(ShopContext)
 
   return (
     <div className='flex item-center justify-between py-5 font-medium'>
@@ -29,8 +32,10 @@ const Navbar = () => {
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
         </NavLink>
       </ul>
+
       <div className="flex items-center gap-6">
-        <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+        <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+
         <div className="group relative">
           <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
@@ -41,12 +46,14 @@ const Navbar = () => {
             </div>
           </div>
         </div>
+
         <Link to='/Cart' className='relative'>
           <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
           <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
         </Link>
         <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
       </div>
+
       {/* Sidebar menu for small screen */}
       <div className={`absolute top-0 right-0 butom-0 overflow-hidden bg-white transistion-all ${visible ? 'w-full' : 'w-0'}`}>
         <div className="flex flex-col text-gray-600">
@@ -54,6 +61,7 @@ const Navbar = () => {
             <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
             <p>Back</p>
           </div>
+
           <NavLink onClick={()=>{setVisible(false)}} className='py-2 pl-6 border' to='/'>HOME</NavLink>
           <NavLink onClick={()=>{setVisible(false)}} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
           <NavLink onClick={()=>{setVisible(false)}} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
---
+++ src/components/RelatedProducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RelatedProducts = (category, subCategory) => {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RelatedProducts
